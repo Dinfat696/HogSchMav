4e9cdbd7c4af6b861d0237e6d8c3bae72c1c0d0b
dinarfathrahmanov <dinfat@mail.ru>
2023-08-16 22:36:23 +0300

    done

--- src/main/java/ru/hogwarts/school/reposirory/AvatarRepository.java
+++ src/main/java/ru/hogwarts/school/reposirory/AvatarRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Student;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AvatarRepository extends JpaRepository<Avatar,Long> {
     Optional<Avatar> findFirstByStudent(Student student);
+    List<Avatar> findAll();
+
+
 }
